445bdba44a4b638aa63ac4ed48edb62a646e5140
Umidbek Karimov <uma.karimov@gmail.com>
2020-10-31 01:25:22 +0500

    WIP

--- deploy-preview/index.ts
+++ deploy-preview/index.ts
@@ -6,11 +6,13 @@ import { format } from 'util';
 
 async function main() {
   const pr = getInput('pr', { required: true });
+  const defaultAlias = `preview-${pr}`;
   const dir = getInput('dir', { required: true });
-  const alias = getInput('alias') || `preview-${pr}`;
+  const alias = getInput('alias') || defaultAlias;
   const token = getInput('token', { required: true });
   const netlifyToken = getInput('netlify-token', { required: true });
   const netlifySiteID = getInput('netlify-site-id', { required: true });
+  const label = alias === defaultAlias ? '' : alias;
 
   let deployJSON = '';
 
@@ -43,8 +45,8 @@ async function main() {
   await sendReport({
     pr,
     token,
+    label,
     content,
-    label: alias,
     title: 'Preview is ready!',
   });
 }
