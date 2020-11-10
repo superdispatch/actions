392793e60f5efe65d9aeaa01efd1910fa1061c27
Umidbek Karimov <uma.karimov@gmail.com>
2020-11-10 16:43:10 +0500

    fix(build-size): [formatRow] Show previous size for the removed files (2).

--- build-size/limit/dist/index.js
+++ build-size/limit/dist/index.js
@@ -57727,7 +57727,7 @@ var external_util_ = __webpack_require__(1669);
 function toFinite(value) {
     return typeof value == 'number' && Number.isFinite(value) ? value : 0;
 }
-function normalizeDelta(delta, deltaThreshold = 256) {
+function normalizeDelta(delta, deltaThreshold = 128) {
     const absoluteDelta = Math.abs(delta);
     if (absoluteDelta < deltaThreshold)
         return 0;
@@ -57753,10 +57753,10 @@ function getDiffIcon(diff) {
     return '';
 }
 function formatRow(size, delta) {
-    const formattedSize = pretty_bytes_default()(size);
     if (size === 0 && delta < 0) {
-        return [formattedSize, '', 'removed', ''];
+        return [(0,external_util_.format)('~%s~', pretty_bytes_default()(Math.abs(delta))), '', 'removed', ''];
     }
+    const formattedSize = pretty_bytes_default()(size);
     if (delta === size) {
         return [formattedSize, '', 'new file', ''];
     }
