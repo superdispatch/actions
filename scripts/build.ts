4b5c1dd954d4affaedd960cd25f56127d97b7379
Umidbek Karimov <uma.karimov@gmail.com>
2021-01-27 14:16:44 +0500

    chore: Use `esbuild` for bundling.

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,5 @@
 import { exec } from '@actions/exec';
+import { build } from 'esbuild';
 import path from 'path';
 
 async function main() {
@@ -6,19 +7,40 @@ async function main() {
   const [dir] = process.argv.slice(2);
   const rootDir = !dir ? cwd : path.join(cwd, dir);
   const entry = path.join(rootDir, 'index.ts');
-  const out = path.join(rootDir, 'dist');
+  const out = path.join(rootDir, 'dist', 'index.js');
 
-  await exec('rm', ['-rf', out]);
+  await build({
+    bundle: true,
+    platform: 'node',
 
-  await exec('ncc', [
-    'build',
-    entry,
-    '--out',
-    out,
-    '--external',
-    'encoding', // Optional dependency of the `node-fetch`.
-    '--source-map',
-  ]);
+    entryPoints: [entry],
+    outfile: out,
+
+    target: 'node12',
+
+    // define: {
+    //   // Eliminate dead code.
+    //   'process.env.NODE_ENV': JSON.stringify('production'),
+    // },
+  });
+
+  // entryPoints: ['app.js'],
+  //   bundle: true,
+  //   platform: 'node',
+  //   target: ['node10.4'],
+  //   outfile: 'out.js',
+
+  // await exec('rm', ['-rf', out]);
+  //
+  // await exec('ncc', [
+  //   'build',
+  //   entry,
+  //   '--out',
+  //   out,
+  //   '--external',
+  //   'encoding', // Optional dependency of the `node-fetch`.
+  //   '--source-map',
+  // ]);
 }
 
 main().catch((error) => {
