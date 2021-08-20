c9b11d780f6b911a4c5251d1e95a0f1c9379ced1
Doniyor Aliyev <doniyor2109@gmail.com>
2021-08-20 16:12:09 +0500

    feat: Add pr-limit action (#14)

--- README.md
+++ README.md
@@ -121,3 +121,26 @@ jobs:
           skip-recent: 5
           pattern: '^e2e-'
 ```
+
+### PR limit
+
+Limit PRs per user
+
+##### Inputs
+
+- `token` - github token, uses `${{ github.token }}` as default
+- `limit` - number, maximum number of pull requests user can create
+
+##### Usage
+
+```yml
+on:
+  pull_request:
+jobs:
+  pr-limit:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: superdispatch/actions/pr-limit@v1
+        with:
+          limit: 2
+```
---
+++ pr-limit/index.ts
@@ -0,0 +1,50 @@
+import { getInput, group, info, setFailed } from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+
+const limit = Number(getInput('limit', { required: true }));
+const token = getInput('token', { required: true });
+
+async function main() {
+  const octokit = getOctokit(token);
+
+  await group('Checking PR limit', async () => {
+    let userPRCount = 0;
+    const pr = context.payload.pull_request;
+
+    if (!pr) {
+      info('Skipping PR limit');
+      return;
+    }
+
+    const pullRequests = await octokit.request(
+      'GET /repos/{owner}/{repo}/pulls',
+      context.repo,
+    );
+
+    for (const item of pullRequests.data) {
+      if (item.user?.login === context.actor) {
+        userPRCount++;
+      }
+    }
+
+    if (userPRCount > limit) {
+      info('Limit exceeded. Closing PR');
+
+      await octokit.request(
+        'POST /repos/{owner}/{repo}/issues/{issue_number}/comments',
+        {
+          ...context.repo,
+          body: `You can create max ${limit} pull requests at once. Closing PR...`,
+          issue_number: pr.number,
+        },
+      );
+      await octokit.request('PATCH /repos/{owner}/{repo}/pulls/{pull_number}', {
+        ...context.repo,
+        pull_number: pr.number,
+        state: 'closed',
+      });
+    }
+  });
+}
+
+main().catch(setFailed);
