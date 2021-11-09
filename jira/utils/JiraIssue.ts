ca3b978a4c6824d1711a912318d4d302cd31e62b
Doniyor Aliyev <doniyor2109@gmail.com>
2021-11-09 10:18:00 +0500

    fix(jira): Verify issue existence.

--- jira/utils/JiraIssue.ts
+++ jira/utils/JiraIssue.ts
@@ -1,4 +1,27 @@
+import { createClient } from './JiraAPI';
+
+const ISSUE_REGEX = /([a-z]{2,}-\d+)/g;
+
 export function parseIssue(input: string) {
-  const match = /([a-z]{2,}-\d+)/.exec(input);
+  const match = ISSUE_REGEX.exec(input);
   return match?.[1].toUpperCase();
+}
+
+export async function findIssueKey(input: string): Promise<string | null> {
+  const matches = input.match(ISSUE_REGEX);
+
+  if (matches) {
+    const jira = createClient();
+
+    for (const match of matches) {
+      try {
+        const issue = await jira.getIssue(match);
+        return issue.key;
+      } catch (error: unknown) {
+        // ignore issue not found error
+      }
+    }
+  }
+
+  return null;
 }
