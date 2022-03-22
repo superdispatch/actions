53ff8538191828384ba964b5aa1442aa9b6a4fe9
Doniyor Aliyev <doniyor2109@gmail.com>
2022-03-22 14:27:59 +0500

    feat(update-snapshots): Create checks.

--- update-snapshots/index.ts
+++ update-snapshots/index.ts
@@ -1,5 +1,6 @@
 import { getInput, group, info, setFailed, warning } from '@actions/core';
 import { context, getOctokit } from '@actions/github';
+import { GitHub } from '@actions/github/lib/utils';
 import { execOutput } from 'utils/exec';
 
 const message = getInput('message');
@@ -9,6 +10,8 @@ const command = getInput('command', { required: true });
 const updateCommand = getInput('update-command', { required: true });
 
 async function main() {
+  let exitCode = 0;
+
   const octokit = getOctokit(token);
   const branch = process.env.GITHUB_HEAD_REF;
 
@@ -18,25 +21,35 @@ async function main() {
     return;
   }
 
+  const completeCheck = await createCheck(octokit);
+
   try {
     await group('Running command', async () => {
-      const { exitCode } = await execOutput(command);
+      const result = await execOutput(command);
 
-      if (exitCode !== 0) {
+      exitCode = result.exitCode;
+
+      if (result.exitCode !== 0) {
         throw new Error(`Command exited with code ${exitCode}`);
       }
     });
   } catch (error: unknown) {}
 
-  await execOutput('git', ['config', 'user.name', 'github_actions']);
-  await execOutput('git', [
-    'config',
-    'user.email',
-    'github-actions[bot]@users.noreply.github.com',
-  ]);
+  console.log({ exitCode });
 
-  await execOutput('git', ['fetch', '--unshallow', 'origin', branch]);
-  await execOutput('git', ['checkout', '-b', branch, `origin/${branch}`]);
+  if (exitCode === 0) {
+    info('Command successfully finished');
+
+    await completeCheck({
+      conclusion: 'success',
+      output: {
+        title: `"${command}" succeed`,
+      },
+    });
+    return;
+  }
+
+  await checkoutRepo(branch);
 
   await group('Running update command', async () => {
     await execOutput(updateCommand);
@@ -76,6 +89,15 @@ async function main() {
   const { stdout: sha } = await execOutput('git', ['rev-parse', 'HEAD']);
   const commitUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/pull/${context.issue.number}/commits/${sha}`;
 
+  await completeCheck({
+    details_url: commitUrl,
+    conclusion: 'failure',
+    output: {
+      title: `"${command}" failed`,
+      summary: `Snapshots are updated for command: "${command}"`,
+    },
+  });
+
   await octokit.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
@@ -85,6 +107,46 @@ async function main() {
 Snapshots are updated automatically in following commit ${commitUrl}
 Please review before merging.`,
   });
+}
+
+async function checkoutRepo(branch: string) {
+  await execOutput('git', ['config', 'user.name', 'github_actions']);
+  await execOutput('git', [
+    'config',
+    'user.email',
+    'github-actions[bot]@users.noreply.github.com',
+  ]);
+
+  await execOutput('git', ['fetch', '--unshallow', 'origin', branch]);
+  await execOutput('git', ['checkout', '-b', branch, `origin/${branch}`]);
+}
+
+interface CheckDetails {
+  details_url?: string;
+  conclusion?: 'failure' | 'success';
+  output?: {
+    title?: string;
+    summary?: string;
+  };
+}
+
+export async function createCheck(octokit: InstanceType<typeof GitHub>) {
+  const check = await octokit.rest.checks.create({
+    ...context.repo,
+    name: 'Update snapshots',
+    // head_sha: context.payload.pull_request?.head.sha,
+    status: 'in_progress',
+  });
+
+  return async (details: CheckDetails) => {
+    await octokit.rest.checks.update({
+      ...context.repo,
+      check_run_id: check.data.id,
+      completed_at: new Date().toISOString(),
+      status: 'completed',
+      ...details,
+    });
+  };
 }
 
 main().catch(setFailed);
